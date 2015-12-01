3db3326b46fd33770c8b739e7482cc027ca9513c
B3NT1RON <bentleybb@gmail.com>
2015-11-30 22:56:48 -0700

    checking in
    i just checked in...........-__-
    
    Merge branch 'master' of
    https://github.com/creedasaurus/LC3_Sudoku_Solver
    
    Merge remote-tracking branch 'refs/remotes/origin/creeds_check_in'

--- sudoku_sov.asm
+++ sudoku_sov.asm
@@ -11,64 +11,13 @@
 	.ORIG x3000
 
 MAIN
-	LD	R6, STACK
-	JSR	PROMPT
-	JSR	RADICAL
-
-
-
+	
+	LD	R6, STACK 	; Load up stack pointer
+	LD 	R5, BOARD 	; Load up board array pointer
+	JSR	PROMPT 		; Call subroutine for the first prompt and info
+	;JSR	GetAndStore	; Call to subroutine that will get numbers from input and store them
 
 EndMain	Halt
-
-
-
-
-PROMPT	
-	STR	R7, R6, #-1
-	ADD	R6, R6, #-1
-
-	LEA 	R0, name	; Outputs Names & Newline
-	PUTS			; 
-	LEA 	R0, NEWLINE	;
-	PUTS			;
-
-	LEA 	R0, project	; Outputs Project & Newline
-	PUTS			; 
-	LEA 	R0, NEWLINE	;
-	PUTS			;
-
-	LEA 	R0, prompt	; Outputs Prompt
-	PUTS			;
-	
-	LDR	R7, R6, #0
-	ADD	R6, R6, #1
-	RET
-
-	
-
-RADICAL
-
-	STR	R7, R6, #-1	; Save location register
-	ADD	R6, R6, #-1	; Decrement Stack
-
-	AND	R5, R5, #0	; R5 is counter
-	ADD	R5, R5, #4	; LOOP 4 times
-	LEA	R2, array
-
-
-LOOP	GETC			; Test Loop for adding input values into an array
-	PUTC			;
-	STR 	R0, R2, #0	; Stores val of R0 into the loaded array R2[] 
-	ADD	R2, R2, #1	; increments address of array
-	ADD 	R5, R5, #-1	; decrements count
-	BRp	LOOP		;
-	
-	LDR	R7, R6, #0	; Load previous location
-	ADD	R6, R6, #1	; Restore Stack location
-	RET			; Return to location in R7
-	
-
-
 
 
 ;---------------------
@@ -78,18 +27,108 @@ LOOP	GETC			; Test Loop for adding input values into an array
 
 STACK	.FILL	x4000
 
-NEWLINE .STRINGZ "\n"				; new line char in LC-3
+NEWLINE .STRINGZ "\n"	; new line char in LC-3
+
+BOARD	.BLKW	81 #0  ; Test array - has value 0 in each location just as placeholder
+
+
+
+;-------------------
+; PROMPT
+; Subroutine
+; this will display the initial info
+; and prompts
+;--------------------
+PROMPT	
+	STR	R7, R6, #-1
+	ADD	R6, R6, #-1
+
+	LEA 	R0, name	; Outputs Names & Newline
+	PUTS				; 
+	LEA 	R0, NEWLINE	;
+	PUTS			 	;
+
+	LEA 	R0, project	; Outputs Project & Newline
+	PUTS				; 
+	LEA 	R0, NEWLINE	;
+	PUTS 				;
+
+	LEA 	R0, prompt	; Outputs Prompt
+	PUTS				;
+	
+	LDR	R7, R6, #0
+	ADD	R6, R6, #1
+	RET
+
+;-------------------
+; Promp Variables
+;-------------------	
 name	.STRINGZ "Creed :: Tanner :: Brandon's"
 project	.STRINGZ "Sudoku Solver"
 prompt	.STRINGZ "Please input a character: "
+	
 
-OS_KBSR	.FILL	xFE00
-OS_KBDR	.FILL	xFE02
-OS_DSR	.FILL	xFE04
-OS_DDR	.FILL	xFE06
 
-array	.BLKW	5 #3  ; Test array - has value 3 in each location just as placeholder
+;-----------------------------
+; GetAndStore
+; Subroutine
+; *currently proof of concept*
+; This shows an example of how we can
+; get and store numbers for the board
+;----------------------------
 
+GetAndStore
+
+	STR	R7, R6, #-1	; Save location register
+	ADD	R6, R6, #-1	; Decrement Stack
+
+	AND	R5, R5, #0	; R5 is counter
+	ADD	R5, R5, #4	; LOOP 16 times
+	LEA	R2, BOARD
+
+
+LOOP 	GETC				; Test Loop for adding input values into an array
+		PUTC				;
+		STR 	R0, R2, #0	; Stores val of R0 into the loaded array R2[] 
+		ADD 	R2, R2, #1	; increments address of array
+		ADD 	R5, R5, #-1	; decrements count
+		BRp		LOOP		;
+	
+		LDR	R7, R6, #0		; Load previous location
+		ADD	R6, R6, #1		; Restore Stack location
+		RET					; Return to calling location
+		
+
+;------------------------------
+; Subroutine: DISPLAY_BOARD
+;
+; puts the current board config 
+; using whatever is stored in BOARD
+;------------------------------
+
+DISPLAY_BOARD
+	STR R7, R6, #-1	; Save location of call on stack
+	ADD R6, R6, #-1	; 
+
+	LEA	R0, NEWLINE
+	PUTS
+
+	AND R2, R2, #0  ; Counter for moving through the array
+
+DISPLAY_NEXT_VALUE	
+	ADD R3, R5, R2	
+
+
+
+
+;------------------------------
+; BOARD formatting data
+; for displaying the board only
+;------------------------------
+
+	SPACE			.FILL	x20 ; space
+	NEW_LINE		.FILL	x0A ; new line
+	BIG_SPACE		.STRINGZ "   "
 
 
 	.END
