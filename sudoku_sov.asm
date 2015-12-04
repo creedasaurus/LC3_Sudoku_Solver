5cd48c30d8e6a347d693731768516d0f73b34849
Creed Haymond <creedasaurus@gmail.com>
2015-12-03 17:37:25 -0700

    IGNORE-working-this-out
    This is an implementation I need to slowly work into our current file. I
    copied the entire thing in under the .END, so i wouldn't miss anything
    that I had changed. It has some great ideas though in that we can
    pinpoint our SOLVE call and that should link our CHECK subs and
    increments.

--- sudoku_sov.asm
+++ sudoku_sov.asm
@@ -108,6 +108,17 @@ GET_OUT
 	RET					; Return to calling location
 		
 
+
+
+
+
+
+
+
+
+
+
+
 ;------------------------------
 ; Subroutine: DISPLAY_BOARD
 ;
@@ -396,5 +407,303 @@ BOTTOM_BOXES
 
 COLNUM .FILL #1
 ROWNUM .FIll #1
+
+.END
+
+
+
+;==================================================================
+; ------------- DEBUGGING STILL --------------
+;				   * IGNORE *
+; This is some stuff I've been working on. 
+; I think we can implement it in the calling of the sovle functions
+; need to upload it here so I can sync it on my computers and work on it 
+; elsewhere;
+
+	.ORIG x3000
+
+MAIN
+	
+	LD	R6, STACK 	; Load up stack pointer
+	LD 	R5, BOARD 	; Load up board array pointer
+
+	JSR	PROMPT 		; Call subroutine for the first prompt and info
+	JSR	LOAD_BOARD	; Call to subroutine that will get numbers from input and store them
+	;JSR SOLVE_SUDOKU ;Call subroutine that solves the Sudoku
+	
+	;JSR INCR_COLUMN_ROW ; DEBUG
+
+	JSR	DISPLAY_BOARD ;
+	LEA R0, DONE
+	PUTS
+
+EndMain	Halt
+
+
+;---------------------
+; Global data
+;
+;---------------------
+
+STACK	.FILL	x4000
+DONE	.STRINGZ "\n -- done! Exit -- \n"
+NEWLINE .STRINGZ "\n"	; new line char in LC-3
+BOARD	.BLKW	16  ; Test array - has value 0 in each location just as placeholder
+
+
+
+;-------------------
+; PROMPT
+; Subroutine
+; this will display the initial info
+; and prompts
+;--------------------
+PROMPT	
+	STR	R7, R6, #-1 	; 
+	ADD	R6, R6, #-1
+
+	LEA 	R0, name	; Outputs Names & Newline
+	PUTS				; 
+	LEA 	R0, NEWLINE	;
+	PUTS			 	;
+
+	LEA 	R0, project	; Outputs Project & Newline
+	PUTS				; 
+	LEA 	R0, NEWLINE	;
+	PUTS 				;
+
+	LEA 	R0, promptWord	; Outputs Prompt
+	PUTS				;
+	
+	LDR	R7, R6, #0
+	ADD	R6, R6, #1
+	RET
+
+;-------------------
+; Promp Variables
+;-------------------	
+name	.STRINGZ "Creed :: Tanner :: Brandon's "
+project	.STRINGZ "Sudoku Solver"
+promptWord	.STRINGZ "Please input a set of numbers: "
+	
+
+
+
+
+;-----------------------------
+; GetAndStore
+; Subroutine
+; *currently proof of concept*
+; This shows an example of how we can
+; get and store numbers for the board
+;----------------------------
+
+LOAD_BOARD
+
+	STR	R7, R6, #-1	; Save location register
+	ADD	R6, R6, #-1	; Decrement Stack
+
+	AND	R4, R4, #0	; R4 is counter
+	ADD	R4, R4, #8	; LOOP 16 times
+ 	ADD	R4, R4, R4	; "make 16, with R4"
+
+	ADD	R2, R5, #0	; put BOARD pointer in R2
+
+GET_LOOP 	
+	GETC				; Test Loop for adding input values into an array
+	PUTC			;
+	STR 	R0, R2, #0	; Stores val of R0 into the loaded array R2[] 
+	ADD 	R2, R2, #1	; increments address of array\
+	
+	LD		R0, SPACE	
+	OUT
+	
+	ADD 	R4, R4, #-1	; decrements count
+	BRnz	GET_OUT
+	BRp	GET_LOOP		;
+	
+GET_OUT	
+	LDR	R7, R6, #0		; Load previous location
+	ADD	R6, R6, #1		; Restore Stack location
+	RET					; Return to calling location
+		
+
+;------------------------------
+; Subroutine: DISPLAY_BOARD
+;
+; puts the current board config 
+; using whatever is stored in BOARD
+; 
+; Clobbers:
+; R0 = display register
+; R1 = find 0
+; R2 = 0's counted
+;------------------------------
+
+DISPLAY_BOARD
+	STR R7, R6, #-1	; Save location of call on stack
+	ADD R6, R6, #-1	; 
+
+	LEA	R0, NEWLINE
+	PUTS
+
+	AND	R4, R4, #0	; R4 is counter
+	
+	LD	R1, RESET	; give R1, #0
+	NOT	R1, R1		; 
+	ADD	R1, R1, #1	; make - #0 
+
+	AND	R2, R2, #0	; R2 is counter of 0's
+
+DISPLAY_NEXT_VALUE	
+	ADD 	R3, R5, R4	; R5 is the board. Adding value of R4 increments it and loads to R0
+	LDR		R0, R3, #0
+	OUT					; Prints value
+	
+	ADD	R0, R0, R1	; check for 0
+	BRz	INC_ZEROS
+	
+	BRnp	CONTINUE
+
+CONTINUE	
+	ADD	R4, R4, #1	; increment counter
+
+	LD R0, COLUMN
+	ADD R0, R0, #1
+	ST R0, COLUMN
+
+	LD	R0, SPACE
+	OUT
+
+	ADD	R0, R4, #-2
+	BRz	DISPLAY_BIGSPACE
+
+	ADD	R0, R4, #-4
+	BRz	DISPLAY_NEXT_LINE
+
+	ADD	R0, R4, #-6
+	BRz	DISPLAY_BIGSPACE
+
+	ADD	R0, R4, #-8
+	BRz	DISPLAY_NEXT_LINE
+
+	ADD	R0, R4, #-10
+	BRz	DISPLAY_BIGSPACE
+
+	ADD	R0, R4, #-12
+	BRz	DISPLAY_NEXT_LINE
+
+	ADD	R0, R4, #-14
+	BRz	DISPLAY_BIGSPACE
+
+	ADD	R0, R4, #-16
+	BRz	FINISH_DISPLAY
+
+	BRnp	DISPLAY_NEXT_VALUE
+
+	
+;--------------------------
+; INC_ZEROS could also be used to call the subroutine that
+; will begin solving that zero. At this point, the ROW
+; and COLUMN variables will be set properly in order to call
+; the solving subroutine.
+;---------------------------
+INC_ZEROS
+	LD R0, ZERO_COUNT
+	ADD R0, R0, #1
+	ST R0, ZERO_COUNT
+	; CALL SOLVE POSITION SUB
+	BRnzp CONTINUE
+;-----------
+;
+DISPLAY_BIGSPACE
+	LEA	R0, BIG_SPACE
+	PUTS
+	BRnzp	DISPLAY_NEXT_VALUE
+
+DISPLAY_NEXT_LINE
+	LD	R0, NEW_LINE
+	OUT
+	
+	LD R0, RESET
+	ST R0, COLUMN
+
+	LD R0, ROW
+	ADD R0, R0, #1
+	ST R0, ROW
+
+
+	ADD	R0, R4, #-16
+	BRnp	DISPLAY_NEXT_VALUE
+	
+
+FINISH_DISPLAY
+	LD	R0, NEW_LINE
+	OUT
+
+	LD R0, COLUMN
+	ADD R0, R0, #-1
+	OUT
+
+	LD	R0, NEW_LINE
+	OUT
+
+	LD R0, ROW
+	OUT
+
+	LD	R0, NEW_LINE
+	OUT
+
+	LD	R0, ZERO_COUNT
+	OUT
+
+	LDR	R7, R6, #0		; Load previous location
+	ADD	R6, R6, #1		; Restore Stack location
+	RET				; Return to calling location
+
+
+;------------------------------
+; BOARD formatting data
+; for displaying the board only
+;------------------------------
+	SPACE		.FILL	x20 ; space
+	NEW_LINE	.FILL	x0A ; new line
+	BIG_SPACE	.STRINGZ " "
+
+
+
+
+;------------------------------------
+; **** SOLVE_SUDOKU *****
+; subroutine
+;
+; r4 = counter
+; r3 = array pointer
+; 
+;
+;------------------------------------
+
+SOLVE_SUDOKU	
+	STR R7, R6, #-1	; Save location of call on stack
+	ADD R6, R6, #-1	; 
+
+	AND R4, R4, #0  ; initialize the counter at 0
+
+
+
+	
+	LDR	R7, R6, #0		; Load previous location
+	ADD	R6, R6, #1		; Restore Stack location
+	RET				; Return to calling location
+
+
+
+
+zerofound	.STRINGZ	"\nfound zero\n"
+
+COLUMN .FILL x30
+ROW .FIll x30
+RESET .FILL x30
+ZERO_COUNT	.FILL	x30
 
 .END
